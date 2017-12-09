477b9035d2ff2006c3b06f45931a839bf3005800
Chandler De Angelis <chandler.dea@me.com>
2017-12-08 19:08:52 -0800

    Added default implemetation to ViewControllerTransitionPresentationDelegate

--- ViewControllerPresentation/ViewControllerTransitionPresentationDelegate.swift
+++ ViewControllerPresentation/ViewControllerTransitionPresentationDelegate.swift
@@ -12,3 +12,12 @@ public protocol ViewControllerTransitionPresentationDelegate: class {
     func didTapContainer()
 }
 
+extension ViewControllerTransitionPresentationDelegate where Self: UIViewController {
+    
+    public func didTapContainer() {
+        self.presentedViewController?.dismiss(animated: true, completion: .none)
+    }
+}
+
+extension UIViewController: ViewControllerTransitionPresentationDelegate {}
+
